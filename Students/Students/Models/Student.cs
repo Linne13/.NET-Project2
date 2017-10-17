6cf9bb588d07c7f7f0edd53e31442749c34eb49f
Linne13 <a_kiddo13@hotmail.com>
2017-10-17 09:22:16 +0600

    Lab2

---
+++ Students/Students/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "Введите Email.")]
+        [EmailAddress(ErrorMessage = "Неверно введенный Email.")]
+        public string Email { get; set; }
+        [Required]
+        public string City { get; set; }
+    }
+}
